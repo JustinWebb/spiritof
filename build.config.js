37cfbdcbb8097f7baf9e46b80d00ea97ef8cb4a1
Justin Webb <jw@justinwebb.io>
2015-07-03 20:30:43 -0700

    (feat) Add functionality for sass compilation

---
+++ build.config.js
@@ -0,0 +1,17 @@
+/* 
+* @Author: Justin Webb
+* @Date:   2015-07-03 19:45:10
+* @Last Modified by:   Justin Webb
+* @Last Modified time: 2015-07-03 20:17:14
+*/
+
+'use strict';
+
+module.exports  = {
+  dist : 'dist',
+  css_dir : 'dist/assets/css',
+  src_files : {
+    hbs: ['src/**/*.hbs'],
+    sass: ['src/assets/css/**.scss']
+  }
+}
--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,50 @@
 /* 
 * @Author: justinwebb
 * @Date:   2015-07-03 19:07:06
-* @Last Modified by:   justinwebb
-* @Last Modified time: 2015-07-03 19:19:34
+* @Last Modified by:   Justin Webb
+* @Last Modified time: 2015-07-03 20:29:28
 */
 
+// ---------------------------------------------------------
+// Build process dependencies
+// ---------------------------------------------------------
 var gulp = require('gulp');
 var sync = require('run-sequence');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var del = require('del');
+var config = require('./build.config');
 
 
-gulp.task();
+// ---------------------------------------------------------
+// Tasks definitions
+// ---------------------------------------------------------
+
+// --------------- Contiguous Integration ---------------//
+
+gulp.task('clean', function () {
+  del(config.dist);
+});
+
+gulp.task('copy', function () {
+  var targets = config.src_files.hbs;
+  gulp.src(targets)
+    .pipe(gulp.dest(config.dist));
+});
+
+gulp.task('sass', function () {
+  gulp.src(config.src_files.sass)
+    .on('error', sass.logError)
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+        sourcemap: true
+      })
+    )
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(config.css_dir));
+});
+
+// Create deployable files
+gulp.task('build', function (cb) {
+  sync('clean', 'copy', 'sass', cb);
+});
